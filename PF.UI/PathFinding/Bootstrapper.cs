9d3739f7729e304b943f2affbe5492e616f07fb0
ReiderJack <asetonlim@gmail.com>
2020-05-06 21:52:45 +0300

    begining
    set up pages, bootstrapper, added buttons to change pages

---
+++ PF.UI/PathFinding/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using PathFinding.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace PathFinding
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ PF.UI/PathFinding/ViewModels/ShellViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace PathFinding.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+
+        public void LoadDijkstra(object sender, RoutedEventArgs e)
+        {
+            ActivateItem(new DijkstrasViewModel());
+        }
+
+        public void LoadBellmanFord(object sender, RoutedEventArgs e)
+        {
+            ActivateItem(new BellmanFordViewModel());
+        }
+
+        public void LoadFloydWarshall(object sender, RoutedEventArgs e)
+        {
+            ActivateItem(new FloydWarshallViewModel());
+        }
+    }
+}
